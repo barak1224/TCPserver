0cedc1f18a1a9d0e5a4eab714862417c974abf1f
Barak <barak1224@gmail.com>
2017-12-05 20:55:33 +0200

    Server almost finished

--- Server.cpp
+++ Server.cpp
@@ -1,26 +1,15 @@
-/**
- * Created by Josef Ginerman on 04/12/17.
- * ID : 332494830
- **/
-
 #include "Server.h"
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <string.h>
-#include <iostream>
 
 using namespace std;
-#define MAX_CONNECTED_CLIENTS 10
 
 Server::Server(int port) : port(port), serverSocket(0) {
     cout << "Server" << endl;
 }
 
-void Server::start() {
+void Server::initializeServer() {
     // Create a socket point
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1) {
+    if (serverSocket == ERROR) {
         throw "Error opening socket";
     }
     // Assign a local address to the socket
@@ -30,79 +19,93 @@ void Server::start() {
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_addr.s_addr = INADDR_ANY;
     serverAddress.sin_port = htons(port);
-    if (bind(serverSocket, (struct sockaddr
-    *) &serverAddress, sizeof(serverAddress)) == -1) {
+    if (bind(serverSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) == -1) {
         throw "Error on binding";
     }
     // Start listening to incoming connections
     listen(serverSocket, MAX_CONNECTED_CLIENTS);
     // Define the client socket's structures
-    struct sockaddr_in clientAddress;
-    socklen_t clientAddressLen;
+}
+
+int Server::acceptTwoClients() {
+    // Accept a new client connection
+    for (int playerNumber = 0; playerNumber < 2; playerNumber++) {
+        clientSockets[playerNumber] = accept(serverSocket, (struct sockaddr *) &dataClients[playerNumber].clientAddress,
+                                             &dataClients[playerNumber].clientLen);
+        if (clientSockets[playerNumber] < 0) {
+            return ERROR;
+        }
+        cout << "Client " << playerNumber + 1 << " connected" << endl;
+    }
+    return VALID;
+}
+
+void Server::start() {
+    initializeServer();
     while (true) {
         cout << "Waiting for client connections..." << endl;
-        // Accept a new client connection
-        int clientSocket = accept(serverSocket, (struct
-                sockaddr *) &clientAddress, &clientAddressLen);
-        cout << "Client connected" << endl;
-        if (clientSocket == -1)
-            throw "Error on accept";
-        handleClient(clientSocket);
+        if (acceptTwoClients() == ERROR) { throw "Error on accept"; }
+        if (sendPlayersNumbers() == ERROR) { throw "Error on accept"; }
+        bool play = true;
+        while (play) {
+            play = playTurn(clientSockets[0], clientSockets[1]);
+            if (play) {
+                play = playTurn(clientSockets[1], clientSockets[0]);
+            }
+        }
         // Close communication with the client
-        close(clientSocket);
+        close(clientSockets[0]);
+        close(clientSockets[1]);
     }
 }
 
-void Server::handleClient(int clientSocket) {
-    int arg1, arg2;
-    char op;
-    while (true) {
-        // Read new exercise arguments
-        int n = read(clientSocket, &arg1, sizeof(arg1));
-        if (n == -1) {
-            cout << "Error reading arg1" << endl;
-            return;
-        }
-        if (n == 0) {
-            cout << "Client disconnected" << endl;
-            return;
-        }
-        n = read(clientSocket, &op, sizeof(op));
-        if (n == -1) {
-            cout << "Error reading operator" << endl;
-            return;
-        }
-        n = read(clientSocket, &arg2, sizeof(arg2));
-        if (n == -1) {
-            cout << "Error reading arg2" << endl;
-            return;
-        }
-        cout << "Got exercise: " << arg1 << op << arg2 <<
-             endl;
-        int result = calc(arg1, op, arg2);
-        // Write the result back to the client
-        n = write(clientSocket, &result, sizeof(result));
-        if (n == -1) {
+bool Server::playTurn(int clientSocket1, int clientSocket2) {
+    char arr[MAX_MOVE];
+    bool continuePlay;
+    continuePlay = readFrom(clientSocket1, arr);
+    if (strcmp(arr, "END") == 0) {
+        continuePlay = false;
+    } else if (continuePlay) {
+        continuePlay = writeFrom(clientSocket2, arr);
+    }
+    return continuePlay;
+}
+
+// Handle requests from a specific client
+bool Server::readFrom(int clientSocket, char arr[MAX_MOVE]) {
+    // Read new exercise arguments
+    int n = read(clientSocket, arr, sizeof(char) * MAX_MOVE);
+    return checkForErrors(n);
+}
+
+bool Server::writeFrom(int clientSocket, char arr[MAX_MOVE]) {
+    // Read new exercise arguments
+    int n = write(clientSocket, arr, sizeof(char) * MAX_MOVE);
+    return checkForErrors(n);
+}
+
+bool Server::checkForErrors(int n) {
+    if (n == ERROR) {
+        cout << "Error reading chosen move" << endl;
+        return false;
+    }
+    if (n == DISCONNECT) {
+        cout << "Client disconnected" << endl;
+        return false;
+    }
+    return true;
+}
+
+int Server::sendPlayersNumbers() {
+    for (int playerNumber = 0; playerNumber < 2; playerNumber++) {
+        int sendPlayerNumber = playerNumber + 1;
+        int n = write(clientSockets[playerNumber], &sendPlayerNumber, sizeof(sendPlayerNumber));
+        if (n == ERROR) {
             cout << "Error writing to socket" << endl;
-            return;
+            return ERROR;
         }
     }
-}
-
-int Server::calc(int arg1, const char op, int arg2) const {
-    switch (op) {
-        case '+':
-            return arg1 + arg2;
-        case '-':
-            return arg1 - arg2;
-        case '*':
-            return arg1 * arg2;
-        case '/':
-            return arg1 / arg2;
-        default:
-            cout << "Invalid operator" << endl;
-            return 0;
-    }
+    return VALID;
 }
 
 void Server::stop() {
